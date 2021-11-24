c3f5ac046f1176c5bc0868854e5ceafcb1489796
Carolina Nonato <csnonato@gmail.comgit config --global user.email>
2021-11-24 18:48:47 -0500

    Complete step 1: capture user input

--- src/currency-converter.js
+++ src/currency-converter.js
@@ -15,6 +15,13 @@
 
 // We will store each piece of information in a dedicated variable for later use.
 
+const amount = process.argv[2];
+const initialCurrency = process.argv[3];
+const targetCurrency = process.argv[4];
+
+console.log(amount);
+console.log(initialCurrency);
+console.log(targetCurrency);
 
 
 // --------------------------------------------------
